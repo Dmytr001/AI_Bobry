520e148fcf3b2c21cb31340af47009cc9b41d94b
Milosz <km55843@zut.edu.pl>
2026-01-24 13:59:58 +0100

    Dodaj - button

--- PLUSFLIX/src/View/admin/partials/nav.php
+++ PLUSFLIX/src/View/admin/partials/nav.php
@@ -10,6 +10,8 @@ $active = $active ?? '';
   <div style="margin-top:10px;">
     <a href="/admin/movies" <?= $active === 'movies' ? 'style="font-weight:bold;"' : '' ?>>Filmy</a>
     <span> | </span>
+    <a href="/admin/movies/create" <?= $active === 'movies' ? 'style="font-weight:bold;"' : '' ?>>Dodaj</a>
+    <span> | </span>
     <a href="/admin/reviews" <?= $active === 'reviews' ? 'style="font-weight:bold;"' : '' ?>>Oceny</a>
     <span> | </span>
     <a href="/admin/admins/create" <?= $active === 'admins' ? 'style="font-weight:bold;"' : '' ?>>Admini</a>
